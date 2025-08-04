8bba4e811fbec6653ef9ce217712a00d4acdd456
Divya Sri <divyasrimandadi8@gmail.com>
2025-08-04 23:21:42 +0530

    image

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 uploaded_imgs = 'uploads'
 compressed_imgs = 'compressed'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+extensions = {'png', 'jpg', 'jpeg'}
 os.makedirs(uploaded_imgs, exist_ok=True)
 os.makedirs(compressed_imgs, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = uploaded_imgs
-app.config['COMPRESSED_FOLDER'] = compressed_imgs
+app.config['uploaded_imgs'] = uploaded_imgs
+app.config['compressed_imgs'] = compressed_imgs
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in extensions
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        quality = int(request.form.get('quality', 70))
+        quality = int(request.form.get('quality', 0))
         file = request.files.get('image')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
